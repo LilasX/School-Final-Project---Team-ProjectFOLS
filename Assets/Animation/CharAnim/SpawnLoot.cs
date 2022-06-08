9608ce29ae0d232a96daa379366c246467a57c2f
LilasX <LilasX@users.noreply.github.com>
2022-06-08 06:32:10 -0400

    Spawning Coins and Coins animation with SFX
    Created a script to spawn loot and managing coins animation and sounds.
    Made a coin prefab and SpawnLoot script has been added to GoblinMelee

---
+++ Assets/Animation/CharAnim/SpawnLoot.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLoot : MonoBehaviour
+{
+    public List<GameObject> loot = new List<GameObject>();
+    public int minRange = 5;
+    public int maxRange = 10;
+
+    private Transform spawnPos;
+    private bool collected;
+    public bool spawned;
+
+    private void OnValidate()
+    {
+        if(minRange > maxRange)
+        {
+            maxRange = minRange + 1;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPos = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawned && !collected)
+        {
+            spawned = false;
+            TakeLoot();
+        }
+    }
+
+    public void TakeLoot()
+    {
+        collected = true;
+        int num = Random.Range(minRange, maxRange);
+        StartCoroutine(LootSpawning(num));
+    }
+
+    IEnumerator LootSpawning(int number)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for(int i = 0; i< number; i++)
+        {
+            GameObject tempLoot = Instantiate(loot[0]);
+            tempLoot.transform.position = this.spawnPos.position;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
