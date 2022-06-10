832c0f7e2861840258a4497474ac79361097cbdd
jfabianduran <fabian_duran@hotmail.fr>
2022-06-10 04:08:00 -0400

    LevelChanger prefab object for Fading
    added fading effect on the script LoadScene with prefab object (LevelChanger)

--- Assets/Scripts/Menu/LoadScene.cs
+++ Assets/Scripts/Menu/LoadScene.cs
@@ -5,36 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    private AsyncOperation async; //Contenu du chargement.
+
+    public Animator animator;
+
+    private AsyncOperation async;
 
     public void BtnLoadScene(int i) //pas de parametres = charge la scene suivante.
     {
-        if (async != null) return; //Arrete l'execution si async n'est pas nul
-
+        if (async != null) return;
+        animator.SetTrigger("FadeOut");
         StartCoroutine(IELoadSceneInt(i));
     }
 
     public void BtnLoadScene(string s) // s = nom de la scene
     {
         if (async != null) return;
-
+        animator.SetTrigger("FadeOut");
         StartCoroutine(IELoadSceneString(s));
     }
 
     IEnumerator IELoadSceneInt(int i)
     {
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(1f);
         Debug.Log("Play");
         async = SceneManager.LoadSceneAsync(i);
     }
 
     IEnumerator IELoadSceneString(string s)
     {
-        yield return new WaitForSeconds(0.25f);
-        Debug.Log("Play");
+        yield return new WaitForSeconds(1f);
+        
         async = SceneManager.LoadSceneAsync(s);
     }
 
+    private void Update()
+    {
+        
+    }
+
+    //public void OnFadeCompleteString(string s)
+    //{
+    //    Debug.Log(sceneToLoad_string);
+    //    // async = 
+    //    SceneManager.LoadScene(s);
+    //}
+
+    //public void OnFadeCompleteInt()
+    //{
+    //    // async = 
+    //    Debug.Log(sceneToLoad_int);
+    //    SceneManager.LoadScene(sceneToLoad_int);
+    //}
 }
 
 
