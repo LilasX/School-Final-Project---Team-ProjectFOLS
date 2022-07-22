9de2ac0f2e0d0fb158a9cbfd1c89de551fee838a
Oussaro88 <77403466+Oussaro88@users.noreply.github.com>
2022-07-22 13:37:15 -0400

    Correction fire spell + bake navemsh du ritual room
    rajout rigidibody au prefab explosion

--- Assets/Scripts/EnemyScript/BaseProjectile.cs
+++ Assets/Scripts/EnemyScript/BaseProjectile.cs
@@ -40,9 +40,7 @@ public class BaseProjectile : MonoBehaviour
     {
         if (shooter == Shooter.player && other.gameObject.GetComponent<EnemyMain>())
         {
-            //other.gameObject.GetComponent<EnemyMain>().GetCurrentHP -= 20;
             other.gameObject.GetComponent<EnemyMain>().OnHurt(40); //FIRE BURST is instanciated twice
-
         }
         else if (shooter == Shooter.enemy)
         {
